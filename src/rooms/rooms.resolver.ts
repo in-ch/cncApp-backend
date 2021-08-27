902ac97e78e1b3463982ef6abb32faa898221aac
in-ch <sxin2949@naver.com>
2021-08-27 12:22:56 +0900

    채팅방 업로드 subscription 추가

--- src/rooms/rooms.resolver.ts
+++ src/rooms/rooms.resolver.ts
@@ -1,9 +1,12 @@
 import { UseGuards } from '@nestjs/common';
-import { Resolver, Query, Mutation, Args, Int} from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Int, Subscription} from '@nestjs/graphql';
 import { CreateRoomMessagePhotoInput } from './dtos/create-room-message-photo.dto';
 import { CreateRoomInput, CreateRoomOutput } from './dtos/create-room.dto';
 import { Rooms } from './entities/rooms.entity';
 import {RoomsService} from './rooms.services';
+import { PubSub } from 'graphql-subscriptions';
+
+const pubSub = new PubSub();
 
 @Resolver(_ => Rooms)
 export class RoomsResolver {
@@ -11,12 +14,16 @@ export class RoomsResolver {
 
   @Mutation(_ => Rooms)
   async createRoom(@Args('input') createRoomInput: CreateRoomInput): Promise<Rooms> {
-      return await this.roomsService.createRooms(createRoomInput);
+    const result = await this.roomsService.createRooms(createRoomInput);
+    pubSub.publish('roomAdded',{roomAdded: result});  
+    return result;
   }
 
   @Mutation(_ => Rooms)
   async createRoomMessagePhoto(@Args('input') createRoomMessagePhoto: CreateRoomMessagePhotoInput): Promise<Rooms> {
-      return await this.roomsService.createRoomMessagePhoto(createRoomMessagePhoto);
+    const result =  await this.roomsService.createRoomMessagePhoto(createRoomMessagePhoto);
+    pubSub.publish('roomAdded',{roomAdded: result});  
+    return result;
   }
   
   @Query(_ => [Rooms])
@@ -25,6 +32,11 @@ export class RoomsResolver {
       @Args('consultNo', {type: () => Int}) consultNo:number
   ) : Promise<Rooms[]> {
       return await this.roomsService.loadRooms(userNo, consultNo);
+  }
+
+  @Subscription(()=>Rooms)
+  roomAdded() {
+      return pubSub.asyncIterator('roomAdded');
   }
 }
   
